9801857634dc3f7683c35e35e9a581b03dfaea37
Artem <a.ermizin.corp@yandex.ru>
2026-08-22 15:17:54 +0700

    Исправлены замечания, добавлена зависимость kafka-client и использован стандартный kafkaProducer.

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaConfig.java
@@ -1,35 +1,53 @@
 package ru.yandex.practicum.collector.config;
 
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.springframework.beans.factory.annotation.Value;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import ru.yandex.practicum.collector.serializer.GeneralAvroSerializer;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Configuration
 public class KafkaConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
+    private KafkaProducer<String, SpecificRecordBase> kafkaProducer;
+
     @Bean
-    public ProducerFactory<String, byte[]> producerFactory() {
+    public KafkaProducer<String, SpecificRecordBase> kafkaProducer() {
         Map<String, Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class);
-        return new DefaultKafkaProducerFactory<>(config);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
+        config.put(ProducerConfig.ACKS_CONFIG, "1");
+        config.put(ProducerConfig.RETRIES_CONFIG, 3);
+        kafkaProducer = new KafkaProducer<>(config);
+        log.info("KafkaProducer создан с bootstrap.servers = {}", bootstrapServers);
+        return kafkaProducer;
     }
 
-    @Bean
-    public KafkaTemplate<String, byte[]> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+    @PreDestroy
+    public void closeProducer() {
+        if (kafkaProducer != null) {
+            try {
+                kafkaProducer.flush();
+                log.info("Закрытие kafkaProducer таймаутом 30 секунд.");
+                kafkaProducer.close(Duration.ofSeconds(30));
+                log.info("KafkaProducer успешно закрыт.");
+            } catch (Exception ex) {
+                log.error("Ошибка при закрытии KafkaProducer: {}.", ex);
+            }
+        }
     }
 }
--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/service/KafkaSenderServiceImpl.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/service/KafkaSenderServiceImpl.java
@@ -2,15 +2,10 @@ package ru.yandex.practicum.collector.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.avro.io.BinaryEncoder;
-import org.apache.avro.io.DatumWriter;
-import org.apache.avro.io.EncoderFactory;
-import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.avro.specific.SpecificRecordBase;
-import org.apache.kafka.common.errors.SerializationException;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.yandex.practicum.collector.mapper.HubEventMapper;
 import ru.yandex.practicum.collector.mapper.SensorEventMapper;
 import ru.yandex.practicum.collector.model.hub.HubEvent;
@@ -18,15 +13,11 @@ import ru.yandex.practicum.collector.model.sensor.SensorEvent;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 @Slf4j
 public class KafkaSenderServiceImpl implements KafkaSenderService {
-    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+    private final KafkaProducer<String, SpecificRecordBase> producer;
     private final HubEventMapper hubMapper;
     private final SensorEventMapper sensorMapper;
 
@@ -36,32 +27,24 @@ public class KafkaSenderServiceImpl implements KafkaSenderService {
     @Override
     public void sendHubEvent(HubEvent event) {
         HubEventAvro avroEvent = hubMapper.toAvro(event);
-        byte[] serialized = serialize(HUBS_TOPIC, avroEvent);
-        kafkaTemplate.send(HUBS_TOPIC, event.getHubId(), serialized);
-        log.info("Событие отправлено в топик {}", HUBS_TOPIC);
+        ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(
+                HUBS_TOPIC,
+                null,
+                event.getTimestamp().toEpochMilli(),
+                event.getHubId(),
+                avroEvent);
+        producer.send(record);
     }
 
     @Override
     public void sendSensorEvent(SensorEvent event) {
         SensorEventAvro avroEvent = sensorMapper.toAvro(event);
-        byte[] serialized = serialize(SENSORS_TOPIC, avroEvent);
-        kafkaTemplate.send(SENSORS_TOPIC, event.getHubId(), serialized);
-        log.info("Событие отправлено в топик {}", SENSORS_TOPIC);
-    }
-
-    private byte[] serialize(String topic, SpecificRecordBase data) {
-        if (data == null) {
-            return null;
-        }
-
-        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-            BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(out, null);
-            DatumWriter<SpecificRecordBase> writer = new SpecificDatumWriter<>(data.getSchema());
-            writer.write(data, encoder);
-            encoder.flush();
-            return out.toByteArray();
-        } catch (IOException ex) {
-            throw new SerializationException("Ошибка сериализации данных для топика [" + topic + "]", ex);
-        }
+        ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(
+                SENSORS_TOPIC,
+                null,
+                event.getTimestamp().toEpochMilli(),
+                event.getHubId(),
+                avroEvent);
+        producer.send(record);
     }
 }
